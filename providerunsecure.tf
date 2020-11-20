404f89942780c4a662a958e7a68c9363d6566b7f
Sahay-ohikc <71588899+Sahay-ohikc@users.noreply.github.com>
2020-11-20 15:06:27 +0200

    Update providerunsecure.tf

--- providerunsecure.tf
+++ providerunsecure.tf
@@ -1,6 +1,7 @@
 provider "aws" {
+  profile = "default"
   region  = "us-east-1"
   aws_access_key_id=AKIARLME3RKA5ZPVL5HK
- aws_secret_access_key=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
+  aws_secret_access_key=7AnYkHTso/X8pGCYvZqehmHXgoKj6s2cBhh++p47
 }
 
