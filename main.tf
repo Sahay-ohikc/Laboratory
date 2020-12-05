2e0c2740d29240c5bd56457b5b44b104448cdbda
Oleksandr Khabarov <71588899+Sahay-ohikc@users.noreply.github.com>
2020-12-06 00:10:09 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -23,7 +23,7 @@ provider "kubernetes" {
   host                   = data.aws_eks_cluster.cluster.endpoint
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
   token                  = data.aws_eks_cluster_auth.cluster.token
-  load_config_file       = true
+  load_config_file       = false
   version                = "~> 1.11"
 }
 
@@ -55,6 +55,7 @@ module "eks" {
   }
 
   write_kubeconfig   = true
+  manage_aws_auth = false
   config_output_path = "./"
 }
 
